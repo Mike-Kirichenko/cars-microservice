df39ceefecc8b0a88ed6beb1a00c8883884ada93
M-kirichenko <m.kirichenko.exceedteam@gmail.com>
2022-12-05 17:10:00 +0400

    reading by chunks

--- src/CarsImport/carsImport.controller.ts
+++ src/CarsImport/carsImport.controller.ts
@@ -25,11 +25,14 @@ export class CarsImportController {
           callback(null, filename);
         },
       }),
-      fileFilter: (req, file, cb) => {
+      fileFilter: (req, file, callback) => {
         const ext = extname(file.originalname);
         if (ext !== '.csv')
-          return cb(new BadRequestException('Invalid file extension'), false);
-        return cb(null, true);
+          return callback(
+            new BadRequestException('Invalid file extension'),
+            false,
+          );
+        return callback(null, true);
       },
     }),
   )
@@ -37,6 +40,6 @@ export class CarsImportController {
     @UploadedFile()
     file: Express.Multer.File,
   ) {
-    return file;
+    return this.carsImportService.parseByChunks(file.path);
   }
 }
--- src/CarsImport/carsImport.service.ts
+++ src/CarsImport/carsImport.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { createReadStream } from 'fs';
+import { parse } from 'csv-parse';
 
 @Injectable()
 export class CarsImportService {
-  public carsImportServiceCall() {
-    return 'ok';
+  public parseByChunks(path: string) {
+    createReadStream(path)
+      .pipe(parse({ delimiter: ',' }))
+      .on('data', function (row) {
+        console.log(row.toString());
+      })
+      .on('end', function () {
+        console.log('finished');
+      })
+      .on('error', function (error) {
+        console.log(error.message);
+      });
   }
 }
